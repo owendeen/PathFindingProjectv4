f2646a03f810f6e9d61527c619dc935c6d5c03d2
zackl18 <ZSLewis18@gmail.com>
2023-04-06 15:57:34 -0400

    Node var initialization xy - Zack L

--- src/main/java/com/example/pathfindingprojectv4/Node.java
+++ src/main/java/com/example/pathfindingprojectv4/Node.java
@@ -1,26 +1,10 @@
 package com.example.pathfindingprojectv4;
 
 public class Node {
-    private boolean up;
-    private boolean down;
-    private boolean left;
-    private boolean right;
-    private boolean diagUR;
-    private boolean diagUL;
-    private boolean diagDR;
-    private boolean diagDL;
-    private boolean visited;
-    private boolean solved;
-    public Node(boolean up, boolean down, boolean left, boolean right, boolean diagUR, boolean diagUL, boolean diagDR, boolean diagDL, boolean solved, boolean visited){
-        this.up = up;
-        this.down = down;
-        this.left = left;
-        this.right = right;
-        this.diagUR = diagUR;
-        this.diagUL = diagUL;
-        this.diagDR = diagDR;
-        this.diagDL = diagDL;
-        this.solved = solved;
-        this.visited = visited;
+    private int x;
+    private int y;
+    public Node(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
