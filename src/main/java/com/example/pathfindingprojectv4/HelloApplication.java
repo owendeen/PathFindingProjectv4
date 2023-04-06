a9fc69ca080837c0ad37bf64dfb69ae33fb0b326
zackl18 <ZSLewis18@gmail.com>
2023-04-06 14:00:35 -0400

    Test - Zack L

--- src/main/java/com/example/pathfindingprojectv4/HelloApplication.java
+++ src/main/java/com/example/pathfindingprojectv4/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 420, 420);
+        Scene scene = new Scene(fxmlLoader.load(), 69, 96);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
