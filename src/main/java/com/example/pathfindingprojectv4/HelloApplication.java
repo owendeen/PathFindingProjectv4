b2810175a409d0cf3042fe4dfd67e4e8e9ec2ac1
Owen Deen <odeen2021@gmail.com>
2023-04-06 13:25:06 -0400

    test2

--- src/main/java/com/example/pathfindingprojectv4/HelloApplication.java
+++ src/main/java/com/example/pathfindingprojectv4/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.pathfindingprojectv4;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 560, 320);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
