60af201370609ec670c4c982ded20a7bb1e1b1e9
zackl18 <ZSLewis18@gmail.com>
2023-04-06 14:35:40 -0400

    Node variables - Zack L

--- src/main/java/com/example/pathfindingprojectv4/Node.java
+++ src/main/java/com/example/pathfindingprojectv4/Node.java
@@ -1,4 +1,14 @@
 package com.example.pathfindingprojectv4;
 
 public class Node {
+    private boolean up;
+    private boolean down;
+    private boolean left;
+    private boolean right;
+    private boolean diagUR;
+    private boolean diagUL;
+    private boolean diagDR;
+    private boolean diagDL;
+    private boolean visited;
+    private boolean solved;
 }
