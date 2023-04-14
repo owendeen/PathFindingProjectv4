4f4fe6696e4d9f8af496104630431c143623f542
Carson Spitler <css9676@uncw.edu>
2023-04-13 21:51:46 -0400

    Attempting to fix Zack's code

--- src/main/java/com/example/pathfindingprojectv4/HelloApplication.java
+++ src/main/java/com/example/pathfindingprojectv4/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application{
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 490); // 600, 400
-        stage.setTitle("Path Finding Simulator");
+        stage.setTitle("Path Finding Simulator v4");
         stage.setScene(scene);
         stage.show();
     }
